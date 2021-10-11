0d83b811cbb4e0b6eec318d1355e6830672fdbde
oksanastets-code <82946234+oksanastets-code@users.noreply.github.com>
2021-10-11 08:36:34 +0000

    Deploying to gh-pages from @ oksanastets-code/goit-js-hw-10-food-service@5331f9b03e2afdff49b5d7b8e4573e30154e9af0 🚀

--- src.82eeb741.js
+++ src.82eeb741.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.e85a4530.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.82eeb741.js.map
